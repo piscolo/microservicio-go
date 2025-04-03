c4047b4d6d948c5bf9d83ccedef800514654bfce
piscolo <acastroe1979@gmail.com>
2025-04-03 14:30:11 -0300

    mensaje charcha

--- main.go
+++ main.go
@@ -1,23 +1,22 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
+	"fmt"
+	"net/http"
 )
 
 // Manejador para la ruta /saludo
 func saludoHandler(w http.ResponseWriter, r *http.Request) {
-    // Responder con un mensaje en formato JSON
-    w.Header().Set("Content-Type", "application/json")
-    fmt.Fprintf(w, `{"mensaje": "¡Hola, bienvenido al microservicio en Go!"}`)
-    fmt.Fprintf(w, `{"mensaje": "¡Esta es una prueba de la primera modificación :)"}`)
+	// Responder con un mensaje en formato JSON
+	w.Header().Set("Content-Type", "application/json")
+	fmt.Fprintf(w, `{"mensaje": "¡Hola, bienvenido al microservicio en Go!"}`)
 }
 
 func main() {
-    // Asignar la función al endpoint /saludo
-    http.HandleFunc("/saludo", saludoHandler)
+	// Asignar la función al endpoint /saludo
+	http.HandleFunc("/saludo", saludoHandler)
 
-    // Iniciar el servidor en el puerto 8080
-    fmt.Println("Servidor ejecutándose en http://localhost:8080")
-    http.ListenAndServe(":8080", nil)
+	// Iniciar el servidor en el puerto 8080
+	fmt.Println("Servidor ejecutándose en http://localhost:8080")
+	http.ListenAndServe(":8080", nil)
 }
